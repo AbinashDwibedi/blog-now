abce0d99bd609541f4f13eb424d73dbc74c8fac5
= <=>
2025-01-24 10:55:16 +0530

    problem fixed

--- server/index.js
+++ server/index.js
@@ -1,8 +1,8 @@
 import "dotenv/config"
 import {app} from "./src/app.js";
 import {connectDB} from "./src/db/db.js"
-import https from "https"
-import fs from 'fs'
+// import https from "https"
+// import fs from 'fs'
 
 
 // const options = {
@@ -12,12 +12,12 @@ import fs from 'fs'
 
 connectDB().then(()=>{
     try {
-        // app.listen(process.env.PORT,()=>{
-        //     console.log(`Server connected successfully: http://localhost:${process.env.PORT}`);
-        // })
-        https.createServer(app).listen(process.env.PORT,()=>{
-            console.log(`Server connected successfully: https://localhost:${process.env.PORT}`)
+        app.listen(process.env.PORT,()=>{
+            console.log(`Server connected successfully: http://localhost:${process.env.PORT}`);
         })
+        // https.createServer(app).listen(process.env.PORT,()=>{
+        //     console.log(`Server connected successfully: https://localhost:${process.env.PORT}`)
+        // })
     } catch (error) {
         console.log("mongodb connection failed: ",error);
         process.exit(1);
