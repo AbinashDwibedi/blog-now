1f11c59c8e1d43d6f75c92ad7f100924a3c1a17a
= <=>
2025-01-24 11:32:40 +0530

    problem fixed

--- server/src/app.js
+++ server/src/app.js
@@ -8,16 +8,7 @@ const allowedOrigins = [
 ];
 
 app.use(
-  cors({
-    origin: function (origin, callback) {
-      if (allowedOrigins.indexOf(origin) !== -1 || !origin) {
-        callback(null, true);
-      } else {
-        callback(new Error("Not allowed by CORS"));
-      }
-    },
-    credentials: true,
-  })
+  cors()
 );
 app.use(cookieParser());
 app.use(express.json());
