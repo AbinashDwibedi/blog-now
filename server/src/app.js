1b96d4c2e29c6850b56333262cd2f63ae0442023
= <=>
2025-01-24 12:01:58 +0530

    some problems fixed

--- server/src/app.js
+++ server/src/app.js
@@ -9,22 +9,18 @@ const allowedOrigins = [
   "https://blogging-now.web.app",
 ];
 
-// app.use(
-//   cors({
-//     origin: function (origin, callback) {
-//       if (allowedOrigins.indexOf(origin) !== -1 || !origin) {
-//         callback(null, true);
-//       } else {
-//         callback(new Error("Not allowed by CORS"));
-//       }
-//     },
-//     credentials: true,
-//   })
-// );
-app.use(cors({
-    origin:"*",
-    
-}))
+app.use(
+  cors({
+    origin: function (origin, callback) {
+      if (allowedOrigins.indexOf(origin) !== -1 || !origin) {
+        callback(null, true);
+      } else {
+        callback(new Error("Not allowed by CORS"));
+      }
+    },
+    credentials: true,
+  })
+);
 app.use(cookieParser());
 app.use(express.json());
 app.use(express.static("public"));
