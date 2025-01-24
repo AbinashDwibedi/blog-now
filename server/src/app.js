ae4766c00adf47e318d113da57cf2f1f375c4111
= <=>
2025-01-24 11:31:01 +0530

    problem fixed

--- server/src/app.js
+++ server/src/app.js
@@ -1,39 +1,42 @@
 import express from "express";
 import cors from "cors";
 import cookieParser from "cookie-parser";
-import fs from "fs"
+import fs from "fs";
+const allowedOrigins = [
+  "http://localhost:5173",
+  "https://blogging-now.web.app",
+];
 
-const app = express();
-const allowedOrigins = ["http://localhost:5173", "https://blogging-now.web.app"];
-app.use(cors({
-    origin: (origin, callback) => {
-        if (allowedOrigins.includes(origin) || !origin) {
-            callback(null, true);
-        } else {
-            callback(new Error("Not allowed by CORS"));
-        }
+app.use(
+  cors({
+    origin: function (origin, callback) {
+      if (allowedOrigins.indexOf(origin) !== -1 || !origin) {
+        callback(null, true);
+      } else {
+        callback(new Error("Not allowed by CORS"));
+      }
     },
-    credentials: true
-}));
-app.use(cookieParser())
+    credentials: true,
+  })
+);
+app.use(cookieParser());
 app.use(express.json());
 app.use(express.static("static"));
-app.use(express.urlencoded({extended: true}));
+app.use(express.urlencoded({ extended: true }));
 // app.use(globalErrorHandler);
 
+app.get("/", (req, res) => {
+  res.send("Sever is rocking");
+});
 
-app.get("/",(req,res)=>{
-    res.send("Sever is rocking");
-})
+import userRoute from "./routes/user.routes.js";
+import blogRoute from "./routes/blog.routes.js";
+import likeRoute from "./routes/like.routes.js";
+import commentRoute from "./routes/comment.routes.js";
 
-import userRoute from "./routes/user.routes.js"
-import blogRoute from "./routes/blog.routes.js"
-import likeRoute from "./routes/like.routes.js"
-import commentRoute from "./routes/comment.routes.js"
+app.use("/api/v1/user", userRoute);
+app.use("/api/v1/blog", blogRoute);
+app.use("/api/v1/like", likeRoute);
+app.use("/api/v1/comment", commentRoute);
 
-app.use("/api/v1/user",userRoute);
-app.use("/api/v1/blog",blogRoute);
-app.use("/api/v1/like",likeRoute);
-app.use("/api/v1/comment",commentRoute);
-
-export {app};
+export { app };
